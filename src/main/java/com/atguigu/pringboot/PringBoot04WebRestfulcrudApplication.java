c60d1b1d73a82878213a4d5c893482ffcc09a30e
jinchen <544509466@qq.com>
2019-12-11 16:44:18 +0800

    idea整合GitHub测试

---
+++ src/main/java/com/atguigu/pringboot/PringBoot04WebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.pringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringBoot04WebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PringBoot04WebRestfulcrudApplication.class, args);
+    }
+
+}
